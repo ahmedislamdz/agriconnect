d27b82117182fd0e51ead32efc801ea4b07f837c
ahmedislam <b1998ai@gmail.com>
2023-12-18 20:44:43 +0100

    Update build files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals'; // Import reportWebVitals
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+reportWebVitals(); // Call reportWebVitals
