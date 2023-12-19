1c22af1fb359f225892dc3e1ccc9dd18697e3f40
ahmedislam <b1998ai@gmail.com>
2023-12-19 13:04:04 +0100

    Update build files 0.0.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// App.js
-import React from 'react';
-import { Navigate, createBrowserRouter, createHashRouter, useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
 import Basket from './components/Basket/Basket';
 import ContextFilter from './components/Context/ContextFilter';
 import ContextProvider from './components/Context/ContextProvider';
@@ -9,10 +7,8 @@ import FavoritePage from './components/Favorite/FavoritePage';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Products from './components/Products/Products';
+import Footer from './components/Footer/Footer '
 
-// استخدام createBrowserRouter أو createHashRouter
-const BrowserRouter = createBrowserRouter();
-// const HashRouter = createHashRouter();
 
 function App() {
   let router = useRoutes([
@@ -22,24 +18,16 @@ function App() {
     { path: '/favorite', element: <FavoritePage /> },
     { path: '/basket', element: <Basket /> },
     { path: '*', element: <Navigate to={'/'} /> },
-  ]);
-
+  ])
   return (
     <ContextProvider>
       <ContextFilter>
         <Header />
         {router}
+        <Footer />
       </ContextFilter>
     </ContextProvider>
   );
 }
 
-function Root() {
-  return (
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
-}
-
-export default Root;
+export default App;
